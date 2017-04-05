8eb987b241bbab8c7eecb0902d56042388036701
ChechilleDarm\Nayoun Seo <puremint09@gmail.com>
2017-04-06 01:39:36 +0900

    -

--- Elice/Cheshire_high01.py
+++ Elice/Cheshire_high01.py
@@ -1,32 +1,9 @@
 '''
 # 2017. 01 Cheshire's quiz high 01 - The last Winner
 # 2017. 04. 05. Nayoun
-
-# To consider : 
-#   circular list (?) 다행히 python은 -1 인덱스를 지원하고 나에겐 % 가..
-#   binary tree...? 배꼽이 태평양만한것 같은데..?
-#   dictionary ? ordered dict?
 '''
+
 # anim : animal
-
-class node:
-  def __init__(self, numberTicket):
-    self.number = numberTicket
-    self.preTckt = None
-    self.nextTckt = None
-
-  def checkTicket():
-    return number
-
-  def nextTicket():
-    return nextTckt
-
-  def preTicket():
-    return preTckt
-
-def insertTicket(node, linked):
-  print()
-
 def dodoStick():
   # init
   num_anim = 100
@@ -44,8 +21,6 @@ def dodoStick():
 
   for winner in l_anim:
     print("The winner is : " + str(winner + 1))
-
-
 
 if __name__ == "__main__" :
   dodoStick()
