323023200143068b0b4610b8a258a54537921acd
ChechilleDarm\Nayoun Seo <puremint09@gmail.com>
2017-04-06 11:34:25 +0900

    Chesire 01 general algorithm update

--- Elice/Cheshire_high01.py
+++ Elice/Cheshire_high01.py
@@ -2,6 +2,7 @@
 # 2017. 01 Cheshire's quiz high 01 - The last Winner
 # 2017. 04. 05. Nayoun
 '''
+import math
 
 # anim : animal
 def dodoStick():
@@ -20,5 +21,17 @@ def dodoStick():
   for winner in l_anim:
     print("The winner is : " + str(winner + 1))
 
+def binaryBit(num):
+  return int(math.log(num, 2)) # 2진수로 표현했을 때 가장 큰 단위
+
+def general_dodoStick(num_anim):
+  bit = binaryBit(num_anim) # 6
+  # remainder 만큼의 방출자(한 번호 건너)가 생긴 후 첫 번호가 승자가 된다.
+  remainder = num_anim - math.pow(2, bit)
+  winner = 2 * remainder + 1 # + 1 : remainder 만큼 게임하고 막대를 넘겨받는 번호
+  return winner
+
 if __name__ == "__main__" :
   dodoStick()
+  winner = general_dodoStick(100)
+  print("The winner is " + str(int(winner)))
