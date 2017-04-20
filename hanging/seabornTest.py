d19d7a49ee4604d6583839bca002995a8fa4c347
ChechilleDarm\Nayoun Seo <puremint09@gmail.com>
2017-04-20 22:12:01 +0900

    희경이가 입술 색을 바꿔달랬고ㅋㅋㅋ D4All lecture04

--- hanging/seabornTest.py
+++ hanging/seabornTest.py
@@ -9,9 +9,18 @@ def main():
     print(data[i], data[i + 1], data[i + 2], data[i + 3], data[i + 4], end=' ')
     print(data[i + 5], data[i + 6], data[i + 7], data[i + 8], data[i + 9])
 
+
+  # sns.palplot(sns.color_palette("OrRd", 10))
+  # sns.palplot(sns.color_palette("Spectral", 10))
+  # sns.palplot(sns.color_palette("RdYlBu", 10))
+  sns.set_palette(sns.color_palette("Spectral"))
+
   # sns.boxplot(data)
   # sns.kdeplot(np.array(data))
   sns.violinplot(data)
+  
+  # sns.violinplot(data=data, palette="Spectral")
+
   sns.plt.show()
 
 if __name__ == "__main__":
