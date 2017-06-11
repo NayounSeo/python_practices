6bbcd897e7a56562000b29be6911fd431b63f627
NayounSeo <parpe94@naver.com>
2017-06-11 20:12:55 +0900

    프듀101 관련 완성

--- hanging/Produce101/README.md
+++ hanging/Produce101/README.md
@@ -1,7 +1,13 @@
 # PRODUCE 101 Season 2
 
- 1. crawls hits and like of videos in the playlist (beautifulSoup4)
- 2. result of crawling as playLists_datetime.txt (about every hour)
+ 1. Crawling hits and like of videos in the NaverTV playlist (beautifulSoup4)
+
+ 2. /dist/ : .exe of 1 for Windows task scheduling (using pyinstaller)
+    result of execution is 4
+
+ 3. NaverTv video comments like-clicker (Selenium)
+
+ 4. result of crawling as playLists_datetime.txt (about every hour)
     data of hits and like of every video 
     (Concept performance - 
     NEVER, 열어줘, Oh Little girl, Show Time, I Know You Know)
--- hanging/Produce101/like_clicker.py
+++ hanging/Produce101/like_clicker.py
@@ -1,5 +1,4 @@
 '''
-ASSUMPTION : 네이버 로그인☆이 되어있다 
 GOAL : 
 최민기 직캠 영상 페이지에 접속 후
 - 전체 댓글을 누르고
@@ -11,37 +10,46 @@ GOAL :
 from selenium import webdriver
 import time
 
-url = "http://tv.naver.com/v/1747159/list/132312"
+url_login = "https://nid.naver.com/nidlogin.login"
+url_hit = "http://tv.naver.com/v/1747159/list/132312"
 
 def main():
   binry = "C:\chromedriver_2.29.exe"
   chrome_driver = webdriver.Chrome(binry)
-  chrome_driver.get(url)
+  chrome_driver.get(url_login)
 
-  # TODO : 로그인... 로그인은 중요한 거였어..
-  # 네이버 로그인 예제를 갖다 씁시다... 한글 있었어..
+  # Naver Login
+  iD = chrome_driver.find_element_by_xpath('//*[@id="id"]')
+  # iD.send_keys('로그인할 아이디')
+  psswd = chrome_driver.find_element_by_xpath('//*[@id="pw"]')
+  # psswd.send_keys('로그인 비밀번호')
+  login = chrome_driver.find_element_by_xpath('//*[@id="frmNIDLogin"]/fieldset/input').click()
 
-  time.sleep(3) # TH Yedarm
+  # 댓글 누를 페이지에 접속
+  chrome_driver.get(url_hit)
+  time.sleep(3) # 인터넷 접속 환경에따라 변경
 
-  # chrome_driver.find_element_by_id("id")
-  # chrome_driver.find_elements_by_class_name("id")
-  # chrome_driver.find_element_by_xpath("xpath")
-  # chrome_driver.find_elements_by_css_selector("")
   see_all = chrome_driver.find_element_by_xpath(
                     '//*[@id="cbox_module"]/div/div[5]/div[1]/div/ul/li[2]/a')
   see_all.click() # 전체 댓글 클릭
 
-  time.sleep(3)
+  time.sleep(1)
 
   # 1page 댓글들
   comments = chrome_driver.find_elements_by_css_selector(
       '#cbox_module > div > div.u_cbox_content_wrap > ul > li.u_cbox_comment')
-  for c in comments:
-    c.find_elements_by_css_selector('div.u_cbox_comment_box > div > div.u_cbox_tool > div > a.u_cbox_btn_recomm')
-    print(c)
-    c.click()
 
-  # chrome_driver.quit()
+  # time.sleep(1)
+  # 팝업 비디오는 꺼주기
+  popped_video = chrome_driver.find_element_by_xpath('//*[@id="container"]/div[2]/div/div[1]/div[1]/a').click()
+
+  for c in comments:
+    like = c.find_elements_by_css_selector('div.u_cbox_comment_box > div > div.u_cbox_tool > div > a.u_cbox_btn_recomm')
+    if ("u_cbox_btn_recomm_on" not in like[0].get_attribute("class")):
+      like[0].click()
+      time.sleep(1) # 너무 빨라서 안눌리더라..^^
+
+  chrome_driver.quit()
 
 if __name__ == "__main__":
   main()
